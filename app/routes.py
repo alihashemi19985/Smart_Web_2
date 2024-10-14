f536b97139c532cb1ef202626612e8605143082d
atefehjn <atefe_hajian97@yahoo.com>
2024-10-14 14:16:46 +0330

    design home, login and register page

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,15 @@ bp = Blueprint("main", __name__)
 @bp.route("/")
 def home():
     return render_template("home.html")
+
+
+@bp.route("/register", methods=["POST", "GET"])
+def register():
+    # code here
+    return render_template("register.html")
+
+@bp.route("/login", methods=["POST", "GET"])
+def login():
+    #code here
+    return render_template("login.html")
+
