b44acb0c48d80da31e75e341bd388c588b7558bc
atgharib <atiye_gharib@yahoo.com>
2024-10-21 01:51:28 +0330

    task 2 is completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from . import routes
+from .database import User, db , Prediction, bcrypt
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
-    from . import routes
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+    db.init_app(app)
+    bcrypt.init_app(app)
 
     app.register_blueprint(routes.bp)
 
